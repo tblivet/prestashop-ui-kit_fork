6e56eea305e641ae718714a1278cc59a1f22e9d5
tblivet <thibault.blivet@prestashop.com>
2024-10-02 17:59:36 +0200

    fix: WIP

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,10 +3,9 @@ const path = require('path');
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.js'],
   addons: [
-    '@storybook/addon-a11y',
-    {
-      name: '@storybook/addon-essentials',
-    },
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-docs',
   ],
   webpackFinal: (config) => {
     config.module.rules[3].use = 'html-loader?minimize=false';
